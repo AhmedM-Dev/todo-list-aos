e761b8cd71c4c90f9e8c93acf01751d70674ff35
Ahmed Mahmoudi <ahmed.mahmoudi@protonmail.com>
2021-06-01 19:09:50 +0100

    Added mutation for adding new users

--- src/resolvers/User.ts
+++ src/resolvers/User.ts
@@ -1,7 +1,23 @@
+import { v4 as uuidv4 } from 'uuid'
 import { getMongoRepository, MongoRepository } from 'typeorm'
-import { Arg, Query, Resolver } from 'type-graphql'
+import { Arg, Query, Resolver, InputType, Field, Mutation } from 'type-graphql'
+import cryptyo from 'crypto'
 
 import { User } from '../model/User'
+
+import { Role } from '../enums'
+
+@InputType({ description: 'New user data' })
+class AddUserInput implements Partial<User> {
+  @Field()
+  email: string
+
+  @Field()
+  username: string
+
+  @Field({ defaultValue: Role.BASIC, nullable: true })
+  role?: Role
+}
 
 @Resolver(User)
 export class UserResolver {
@@ -21,5 +37,19 @@ export class UserResolver {
     console.log('user', user)
 
     return user
+  }
+
+  @Mutation(() => User, { nullable: true })
+  async addUser(@Arg('data') newUserData: AddUserInput): Promise<User | undefined> {
+    const newUserId = uuidv4()
+
+    const result = await this.userRepository.insertOne({
+      _id: newUserId,
+      id: newUserId,
+      ...newUserData,
+      password: cryptyo.randomBytes(8).toString('hex')
+    })
+
+    return result?.ops?.[0]
   }
 }
